cd6caf12890f201e9917583e14e3889b65f261cc
Bruna Ribeiro <brrunarib@gmail.com>
2025-07-05 12:23:54 +0200

    Add basic styling to dashboard

--- frontend/nicobruno/src/components/Dashboard.js
+++ frontend/nicobruno/src/components/Dashboard.js
@@ -8,9 +8,10 @@ const Dashboard = () => {
   return (
     <div>
       <NavBar className="NavBar" />
+      <div className="content">
       <h1>Nicole & Bruno</h1>
       <h5>SAVE THE DATE</h5>
-      <h2>15|01|2026</h2>
+      <h2>15|01|2026</h2></div>
     </div>
   );
 };
--- frontend/nicobruno/src/components/NavBar.js
+++ frontend/nicobruno/src/components/NavBar.js
@@ -2,16 +2,14 @@ import React from "react";
 
 const NavBar = () => {
     return (
-        <header>
-            <nav>
-                <ul className="menu">
-                    <li><a href="index.html" className="active">Home</a></li>
-                    <li><a href="info.html">Informações</a></li>
-                    <li><a href="rspv.html">Confirmar Presença</a></li>
-                    <li><a href="giftdonations.html">Presentes</a></li>
-                </ul>
-            </nav>
-        </header>
+        <nav>
+            <ul className="menu">
+                <li><a href="index.html" className="active">Home</a></li>
+                <li><a href="info.html">Informações</a></li>
+                <li><a href="rspv.html">Confirmar Presença</a></li>
+                <li><a href="giftdonations.html">Presentes</a></li>
+            </ul>
+        </nav>
     );
 }
 
